89942e2f855753dc92e2780473764f2bcb86f556
nursyahjaya <nursyahjaya@gmail.com>
2022-06-13 18:21:37 +0700

    update content

--- homepage/secret/newTime.js
+++
@@ -1,2 +0,0 @@
-var timeStart="04/17/22 08:47:39";
-var relapse="11";
--- script.js
+++ script.js
@@ -42,36 +42,3 @@ function generatedWriting(name, combination, w) {
 
 
 generatedWriting(NAME, randomString(ALPHABET), writing)
-
-
-function change() {
-  const aboutme = document.getElementById("aboutme")
-  const footer = document.getElementById("footer")
-
-  const en = {
-    aboutme: "about me<br>\
-  hello, nice to meet you i'm nursyah a software engineering student from indonesia. i like learning something\
-  new, i write code with js, java, cpp, python, and more. do you want to know what i have build check my\
-  projects in below, or visit my github. my hobby is <i class=\"fa-solid fa-music\"></i> listen music and <i\
-      class=\"fa-solid fa-code\"></i>\
-  write code.",
-    footer: "build with <i class=\"fa-solid fa-heart\"></i>"
-  }
-  const id = {
-    aboutme: "tentang ku<br>\
-    halo, senang bertemu denganmu, hai aku nursyah, aku seorang mahasiswa rekayasa perangkat lunak asal indonesia. aku senang mempelajari hal baru, aku menulis kode dengan js, java, cpp, python, dan lebih banyak lagi. ingin tau apa yang sudah kubuat, cek dibawah untuk melihat projectku atau kunjungi githubku. hobby ku adalah <i class=\"fa-solid fa-music\"></i> mendengarkan musik dan <i\
-        class=\"fa-solid fa-code\"></i> menulis kode.",
-    footer: "dibuat dengan <i class=\"fa-solid fa-heart\"></i>"
-  }
-
-  if(lang.innerText == "en"){
-    lang.innerHTML = "id"
-    aboutme.innerHTML = id["aboutme"]
-    footer.innerHTML = id["footer"]
-  }else{
-    lang.innerHTML = "en"
-    aboutme.innerHTML = en["aboutme"]
-    footer.innerHTML = en["footer"]
-  }
-}
-
