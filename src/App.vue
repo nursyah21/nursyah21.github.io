e013590bfd0ba473eea930abf05b79ff20eb2aa5
nursyahjaya <nursyahjaya@gmail.com>
2022-08-04 21:56:28 +0700

    update page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,39 @@
-<template>
+<script>
+</script>
 
-  <h1>nursyahjaya</h1>
+<template >
+  <!-- header nursyah -->
+  <div class="flex flex-col text-center h-[100vh] justify-center text-[#0CC5FF] bg-[rgba(12,197,255,6%)]">
+    <h1 class="underline text-[4rem]">NURSYAH</h1>
+    <p>Android Developer / Web Developer</p>
+    
+    <div class="flex justify-center mt-3">
+      <a href="mailto:xxxy65221@gmail.com">
+        <img src="/email.svg" alt="" class="w-[30px] hover:opacity-80" />
+      </a>
+    </div>
+    <div class="absolute bottom-[3rem] ml-[48%] fill-black">
+        <svg  class="fill-[#0CC5FF] hover:fill-[#7edcf8] animate-bounce" width="24" height="15" viewBox="0 0 24 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 14.75L0 2.74998L2.15 0.599976L12 10.5L21.85 0.649975L24 2.79997L12 14.75Z"/></svg>
+    </div>
+  </div>
+
+  <!-- about -->
+  <div class="flex flex-col text-center h-[100vh] justify-center text-[#0CC5FF]">
+    <h1 class="underline text-[4rem]">NURSYAH</h1>
+    <p>Android Developer / Web Developer</p>
+  </div>
+
+  <!-- software used -->
+  <div class="flex flex-col text-center h-[100vh] justify-center text-[#0CC5FF]">
+    <h1 class="underline text-[4rem]">NURSYAH</h1>
+    <p>Android Developer / Web Developer</p>
+  </div>
+
+  <!-- application -->
+  <div class="flex flex-col text-center h-[100vh] justify-center text-[#0CC5FF]">
+    <h1 class="underline text-[4rem]">NURSYAH</h1>
+    <p>Android Developer / Web Developer</p>
+  </div>
 
 </template>
+
