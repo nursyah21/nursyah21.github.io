4555d8d7f5682661e8e9cf5afea62890630146a3
nursyahjaya <nursyahjaya@gmail.com>
2022-12-24 20:39:06 +0700

    move to astro

---
+++ src/pages/post/finance_app_android.md
@@ -0,0 +1,10 @@
+---
+layout: ../../layouts/PostLayout.astro
+title: finance app android
+author: nursyah
+date: 23-12-2022
+description: A simple finance app android with dark theme
+--- 
+
+A simple finance app android with dark theme,
+this i a story of 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 }
