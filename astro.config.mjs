e7fd47d0186290eaa874d50fc12e2cb5737d5c9c
nursyah21 <nursyahjaya@gmail.com>
2024-08-24 15:32:46 +0700

    migrate from old to new post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), tailwind()]
 });
--- dist/archiveweb_vue/nursyah_files/main.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from '/node_modules/.vite/deps/vue.js?v=bc0e8207'
-import '/src/index.css'
-import App from '/src/App.vue'
-
-createApp(App).mount('#app')
